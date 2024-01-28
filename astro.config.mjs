f871b54b49567531a525ab6ec650a97b489c4f0a
CristhianTapiero <cristhiand-tapierop@unilibre.edu.co>
2024-01-27 19:20:32 -0500

    Delete unused images and files, add new dependencies, and update layout and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,26 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.cristhian-tapiero.vercel.app',
   integrations: [tailwind(), icon({
-    include:{
+    include: {
       mdi: ["*"],
       "gravity-ui": ["*"],
       humbleicons: ["*"],
       brandico: ["*"],
-      "devicon-plain": ["*"],
+      "devicon-plain": ["*"]
     }
-  })],
+  }), robotsTxt()],
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es'
   },
   output: 'server',
   adapter: vercel({
-    imageService: true,
-  }),
+    imageService: true
+  })
 });
